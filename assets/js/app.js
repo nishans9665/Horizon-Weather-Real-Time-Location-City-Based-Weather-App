55bde475ffd147167a90e1d6515712d94feff305
nishans9665 <your-mail.nishansameera@gmail.com>
2025-11-18 13:48:53 +0530

    on time laction track

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,10 +27,48 @@ document.getElementById("cityInput").addEventListener("keyup", function (event) 
                 document.getElementById("humidity").textContent = data.current.humidity;
                 document.getElementById("uvindex").textContent = data.current.uv;
                 document.getElementById("visibility").textContent = data.current.vis_km;
-    })
-    .catch (() => {
-        alert("City not fond");
-    })
+            })
+            .catch(() => {
+                alert("City not fond");
+            })
 
-}
+    }
 });
+
+navigator.geolocation.getCurrentPosition(success, error);
+
+function success(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    console.log(lat, lon);
+}
+function error() {
+    alert("Unable to retrieve your location");
+}
+function success(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=0cefa4b38aae4e3595a95844251611&q=${lat},${lon}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            // Example:
+            document.getElementById("city_name").textContent = data.location.name;
+            document.getElementById("temp").textContent = data.current.temp_c + "°C";
+            const iconUrl = "https:" + data.current.condition.icon;
+             document.getElementById("condition").src = iconUrl
+            document.getElementById("country").textContent = data.location.country;
+            document.getElementById("ontime").textContent = data.location.localtime;
+            document.getElementById("condition-live").textContent = data.current.condition.text;
+
+            document.getElementById("windSpeed").textContent = data.current.wind_kph;
+            document.getElementById("cloudcover").textContent = data.current.cloud;
+            document.getElementById("pressure").textContent = data.current.pressure_mb;
+            document.getElementById("humidity").textContent = data.current.humidity;
+            document.getElementById("uvindex").textContent = data.current.uv;
+            document.getElementById("visibility").textContent = data.current.vis_km;
+        });
+}
